34aff213f8e0bb85828f85f9743efc6f446e3193
slayercoder <ms81291@gmail.com>
2017-11-02 01:01:03 +0530

    responsive navbar

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
 var span = document.querySelector("#year");
 var date = new Date();
 span.textContent = date.getFullYear();
+
+
+var hamburgerButton = document.getElementById("hamburgerButton");
+var hamburgerDiv = document.getElementById("hamburgerDiv");
+var displayDiv = false;
+function hamClick(){
+    displayDiv = !displayDiv;
+    if(displayDiv){
+        hamburgerDiv.setAttribute("style", "display : block");
+    }
+    else{
+        hamburgerDiv.setAttribute("style", "display : none");
+    }
+    
+}
+hamburgerButton.addEventListener("click", hamClick);
