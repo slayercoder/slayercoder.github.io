580ff7cda7a24a06d1a0cb286b8eca7621d7be38
slayercoder <ms81291@gmail.com>
2017-12-03 22:57:16 +0530

    svg completed

---
+++ js/script.js
@@ -0,0 +1,88 @@
+/// Module Pattern implementation
+var { hamClick, hideDivOnTap } = (function(){
+    var hamClick = function(element){
+        if(!displayDiv){
+            element.setAttribute("style", "display : block");
+            displayDiv = true;
+
+        }
+        else{
+            element.setAttribute("style", "display : none");
+            displayDiv = false;
+        }
+        
+    };
+
+    var hideDivOnTap = function(element){
+        if(displayDiv){
+            element.style.display = "none";
+            displayDiv = false;
+        }
+    };
+
+    return {
+        hamClick, hideDivOnTap
+    }
+
+})();
+
+//  global variables
+var  body, displayDiv, loaderDiv, hamburgerButton, hamburgerDiv, hamburgerDivLinks, linkLen, span, date;
+
+/// loader while window is loading
+loaderDiv = document.getElementById("background-loader");
+window.onload = function(){
+                    loaderDiv.setAttribute("style", "display:none");
+                }
+
+
+
+// adding tap event globally in a window by capturing the event 
+body = document.getElementsByTagName('body')[0];
+body.addEventListener("click", function(){
+    if(displayDiv){
+        hamburgerDiv.setAttribute("style", "display : none");
+        displayDiv = false;
+    }
+}, false);
+
+
+// global state of hamburger div
+displayDiv = false;
+
+// selected the button element from DOM
+hamburgerButton = document.getElementById("hamburgerButton");
+
+// this 'div' will be displayed on state change from 'false' to 'true'
+hamburgerDiv = document.getElementById("hamburgerDiv");
+
+// these are the links within the above div for internal navigation
+hamburgerDivLinks = document.querySelectorAll(".pageSection");
+
+// added an event listener to the 'div' selected above, also stopping the event bubbling
+hamburgerButton.onclick = function(event){
+                              event.stopPropagation();
+                              hamClick.call(this, hamburgerDiv);
+                          }
+
+// adding a event listener to every link in hamburger menu
+linkLen = hamburgerDivLinks.length;
+for(var i = 0; i < linkLen; i++){
+    hamburgerDivLinks[i].onclick = function(event){
+                                       event.stopPropagation();
+                                       hideDivOnTap.call(this, hamburgerDiv);
+                                    }
+}
+
+// removing the persisting hamburger menu
+window.onresize = function(){
+    if(window.innerWidth > 576){
+        hamburgerDiv.setAttribute("style", "display : none");
+        displayDiv = false;
+    }
+}
+
+// automated year
+span = document.querySelector("#year");
+date = new Date();
+span.textContent = date.getFullYear();
